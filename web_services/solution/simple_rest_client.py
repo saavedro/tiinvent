3e535b0e88de9b7103160e978d6d1e9c4373187f
Piotr Dziuba <piotr.dziuba@posti.com>
2019-06-11 22:41:39 +0200

    ws refactor into functions

--- web_services/solution/simple_rest_client.py
+++ web_services/solution/simple_rest_client.py
@@ -1,4 +1,4 @@
-# This script example of code based interaction with webservices using requests library
+# This script is example of code based interaction with webservices using `requests` library
 # We use free toy service: http://dummy.restapiexample.com
 
 # The service is an online HR system to store data about employees such as:
@@ -13,31 +13,60 @@ import requests
 import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
-# get all employees and show to last added ones
-print("Getting all entries")
-r = requests.get("http://dummy.restapiexample.com/api/v1/employees")
-employees = r.json()
-pp.pprint(employees[-2:]) # Get two last entries
+service_url = "http://dummy.restapiexample.com/api/v1/"
 
-print("Creating new entry")
-payload = {
-    "name": "Batman",
-    "salary": 99999999,
-    "age": 60,
-}
-r = requests.post("http://dummy.restapiexample.com/api/v1/create", json=payload)
-print("Response: {}, {}".format(r.status_code, r.text))
+def get_employees():
+    " Get all employees"
+    print("INFO: Getting all entries")
+    r = requests.get("http://dummy.restapiexample.com/api/v1/employees")
+    employee = r.json()
+    return employee
 
-# let's get our entry id!
-try:
-    _id = r.json()["id"]
-except:
-    print("New entry not created correctly, taking last for further processing")
-    _id = employees[-1]["id"]
+def get_employee(_id):
+    " Gets data of employee of give id from the ws"
+    print("INFO: Getting info about employee id={}".format(_id))
+    r = requests.get("http://dummy.restapiexample.com/api/v1/employee/{}".format(_id))
+    print("INFO: Response: {}, {}".format(r.status_code, r.text))
+    employee = r.json()
+    return employee
 
-print("Getting our employee of interest: {}".format(_id))
-r = requests.get("http://dummy.restapiexample.com/api/v1/employee/{}".format(_id))
-print("Response: {}, {}".format(r.status_code, r.text))
-employee = r.json() 
-pp.pprint(employee)
+def create_employee(name, salary, age):
+    "Creates employee of given data"
+    print("INFO: Creating employee {}".format(name))
+    employee_data = {
+        "name": name,
+        "salary": salary,
+        "age": age,
+    }
+    r = requests.post("http://dummy.restapiexample.com/api/v1/create", json=employee_data)
+    print("INFO: Response: {}, {}".format(r.status_code, r.text))
+    employee = r.json()
+    return employee
 
+def update_employee(_id, name, salary, age):
+    " Update data of given employee "
+    print("INFO: updating employee with id={}".format(_id))
+    employee_new_data = {
+        "name": "Batman",
+        "salary": salary,
+        "age": 60,
+    }
+    r = requests.put("http://dummy.restapiexample.com/api/v1/update/{}".format(_id), json=employee_new_data)
+    print("INFO: Response: {}, {}".format(r.status_code, r.text))
+    employee = r.json()
+    return employee
+
+def delete_employee(_id):
+    " Delete data of given employee"
+    r = requests.delete("http://dummy.restapiexample.com/api/v1/delete/{}".format(_id))
+    print("Response: {}, {}".format(r.status_code, r.text))
+    return r.text
+
+if __name__ == "__main__":
+    pass
+    #employees = get_employees()
+    #pp.pprint(employees[-2:]) # Print two last entries
+
+    #add_employee(
+
+    # Let's give our employee a rise!
